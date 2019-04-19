4671c9560d5caa2d188ebfefdfcad38d32f03da6
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-04-19 11:44:14 -0400

    action wrote for add and get smurf

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,31 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+import axios from 'axios';
+
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+
+export const GET_SMURF_START = 'GET_SMURF_START';
+export const GET_SMURF_SUCCESS = 'GET_SMURF_SUCCESS';
+export const GET_SMURF_FAIL = 'GET_SMURF_FAIL';
+
+
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: GET_SMURF_START });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => dispatch({ type: GET_SMURF_SUCCESS, payload: res.data}))
+    .catch(err => dispatch({ type: GET_SMURF_FAIL, payload: err}))
+}
+
+export const addSmurf = () => dispatch => {
+  dispatch({type: ADD_SMURF_START })
+  axios
+    .post('http://localhost:3333/smurfs')
+    .then(res => dispatch({ type: ADD_SMURF_SUCCESS, payload: res.data }))
+    .catch(err => dispatch({ type: ADD_SMURF_FAIL, payload: err }))
+}
